f678f45ad23b2c2ba1cd1c51faee03045a009f52
Filip Laurentiu <f.laurentiu26@gmail.com>
2022-12-28 12:04:47 +0200

    Spin a database for every cargo test command

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -25,6 +25,7 @@ if [[ -z "${SKIP_DOCKER}" ]]; then
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
+    --name zero2prod-postgres \
     -d postgres \
     postgres -N 1000
 fi
